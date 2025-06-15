51d92403b75cfbf7f28ba3afc6fcb5b398ac6116
SINGHARAJESH <rajeshsingha1947@gmail.com>
2025-06-15 12:36:59 +0530

    this is first version of model

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -20,6 +20,8 @@ logging.basicConfig(
     ]
 )
 
+
+
 # ---------------- Load Model ----------------
 def load_model(model_path: str):
     try:
@@ -69,36 +71,50 @@ def save_metrics(metrics: Dict[str, float], file_path: str) -> None:
         logging.error(f"Failed to save metrics: {e}")
         raise
 
+def save_model_info(run_id:str,model_path:str,file_path:str) -> None:
+    # save the run id and model json path
+    try:
+        model_info = {'run_id':run_id,'model_path':model_path}
+
+        with open(file_path,'w') as path:
+            json.dump(model_info,path,indent=4)
+        logging.debug('model info saved %s',file_path)
+
+    except Exception as e:
+        logging.error('error occoured while saving the model',e)
+
+
 # ---------------- Main Pipeline ----------------
 def main():
 
     mlflow.set_experiment("dvc-pipeline")
-    mlflow.start_run()
-    try:
-        model = load_model('./models/model.pkl')
-        test_df = load_test_data('./data/processed/test_bow.csv')
+    with mlflow.start_run() as run:
+        try:
+            model = load_model('./models/model.pkl')
+            test_df = load_test_data('./data/processed/test_bow.csv')
 
-        X_test = test_df.iloc[:, :-1].values
-        y_test = test_df.iloc[:, -1].values
+            X_test = test_df.iloc[:, :-1].values
+            y_test = test_df.iloc[:, -1].values
 
-        metrics = evaluate_model(model, X_test, y_test)
-        save_metrics(metrics, 'reports/metrics.json')
+            metrics = evaluate_model(model, X_test, y_test)
+            save_metrics(metrics, 'reports/metrics.json')
 
-        # log metrics to mlflow
+            # log metrics to mlflow
 
-        for metric_name,metric_value in metrics.items():
-            mlflow.log_metric(metric_name,metric_value)
+            for metric_name,metric_value in metrics.items():
+                mlflow.log_metric(metric_name,metric_value)
 
-        if hasattr(model,'get_params'):
-            params = model.get_params()
-            for param_name,param_value in params.items():
-                mlflow.log_param(param_name,param_value)
+            if hasattr(model,'get_params'):
+                params = model.get_params()
+                for param_name,param_value in params.items():
+                    mlflow.log_param(param_name,param_value)
 
-        mlflow.sklearn.log_model(model, "model")
+            mlflow.sklearn.log_model(model, "model")
+            save_model_info(run.info.run_id,"model","reports/experiment_info.json")
 
-        logging.info("Model evaluation completed successfully.")
-    except Exception as e:
-        logging.exception("Model evaluation pipeline failed.")
+            logging.info("Model evaluation completed successfully.")
+        except Exception as e:
+            logging.exception("Model evaluation pipeline failed.")
 
 # ---------------- Entry Point ----------------
 if __name__ == '__main__':
---
+++ src/model/register_model.py
@@ -0,0 +1,85 @@
+import json
+import mlflow
+import logging
+import os
+import dagshub
+
+mlflow.set_tracking_uri("https://dagshub.com/rajeshai2000/mlops-mini-project.mlflow")
+dagshub.init(repo_owner='rajeshai2000', repo_name='mlops-mini-project', mlflow=True)
+
+# logging configuration
+logger = logging.getLogger('model_registration')
+logger.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+file_handler = logging.FileHandler('model_registration_errors.log')
+file_handler.setLevel('ERROR')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+def load_model_info(file_path:str) -> dict:
+
+    "load the model info from a json file"
+
+    try:
+        with open(file_path,'r') as file:
+            model_info = json.load(file)
+
+        logger.debug('model loaded from %s',file_path)
+
+        return model_info
+    except FileNotFoundError:
+        logger.error('file not found',file_path)
+        raise
+    except Exception as e:
+        logger.error('unexcepted error occurred while loading the model info: %s',e)
+        raise
+
+
+def register_model(model_name: str,model_info:dict):
+
+    # register the model to the mlflow model registry
+
+    try:
+        model_url = f"runs:/{model_info['run_id']}/{model_info['model_path']}"
+
+        # register the model
+
+        model_version = mlflow.register_model(model_url,model_name)
+
+        client = mlflow.tracking.MlflowClient()
+        client.transition_model_version_stage(
+            name=model_name,
+            version=model_version.version,
+            stage="Staging"
+        )
+        
+        logger.debug(f'Model {model_name} version {model_version.version} registered and transitioned to Staging.')
+    except Exception as e:
+        logger.error('Error during model registration: %s', e)
+        raise
+
+def main():
+
+    try:
+        model_info_path = 'reports/experiment_info.json'
+
+        model_info = load_model_info(model_info_path)
+
+        model_name = 'my_model'
+        register_model(model_name,model_info)
+
+    except Exception as e:
+        logger.error('Failed to complete the model registration process: %s', e)
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
