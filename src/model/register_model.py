6043bef25d123a1fa943823453d67fe5c215eec5
SINGHARAJESH <rajeshsingha1947@gmail.com>
2025-06-22 11:31:22 +0530

    add testing file

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -68,7 +68,7 @@ def register_model(model_name: str,model_info:dict):
         client.transition_model_version_stage(
             name=model_name,
             version=model_version.version,
-            stage="Production",
+            stage="Staging",
             archive_existing_versions=True
         )
         
